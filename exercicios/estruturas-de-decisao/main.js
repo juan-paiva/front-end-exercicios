c6a708d113879548fd34f15a2eaa29add2bf2e80
juan-paiva <juanpaiva.ym@gmail.com>
2025-09-16 12:21:56 -0300

    correção dos exercícios

--- exercicios/estruturas-de-decisao/main.js
+++ exercicios/estruturas-de-decisao/main.js
@@ -3,9 +3,9 @@
 // let numero = prompt('Digite um número: ');
 
 // if (numero > 10) {
-//     console.log('Este número é MAIOR que 10.')
+//     console.log('Este número é MAIOR que 10.');
 // } else {
-//     console.log('Este número não é MENOR que 10.')
+//     console.log('Este número não é MENOR que 10.');
 // }
 
 // 2. Faça um script em JavaScript que solicite um número ao usuário e verifique se ele está entre 532 e 743.
@@ -13,21 +13,25 @@
 // let numeroEntre = prompt('Digite um número: ');
 
 // if (numeroEntre > 532 && numeroEntre < 743 ) {
-//     console.log('O número digitado está entre 532 e 743.')
+//     console.log('O número digitado está entre 532 e 743.');
 // } else {
-//     console.log('O número informado não está entre 532 e 743.')
+//     console.log('O número informado não está entre 532 e 743.');
 // }
 
 // 3. Faça um script em JavaScript que solicite uma senha ao usuário e verifique se ela é igual a uma string 
 // armazenada em uma variável no seu código.
 
-// let senhaCorreta = '2345678'
 // let senha = prompt('Digite sua senha: ');
+// let senhaCorreta = 2345678 // camel-case
+// // let senha_correta = '2345678' // snake-case
+
+// senha = Number(senha); // conversão de variável string para number.
+// console.log(typeof senha);
 
 // if (senha == senhaCorreta) {
-//     console.log('Senha Correta.')
+//     console.log('Login efetuado com sucesso.');
 // } else {
-//     console.log('Senha está errada.')
+//     console.log('Senha incorreta.');
 // }
 
 // 4. Para uma pessoa realizar um consórcio em um banco, é necessário que ela tenha 18 anos ou mais e possua
@@ -35,12 +39,26 @@
 // do usuário e exiba a mensagem se ele está apto ou não realizar um consórcio
 
 // let idade = prompt('Digite sua idade: ');
-// let renda = prompt('Digite sua renda salarial: ');
+
 
 // if (idade >= 18 && renda >= 3036) {
-//     console.log('Você pode fazer um consórcio. ')
+//     console.log('Você pode fazer um consórcio. ');
 // } else {
-//     console.log('Você não pode fazer um consórcio.')
+//     console.log('Você não pode fazer um consórcio.');
+// }
+
+// if ( idade >= 18) {
+//     let renda = prompt('Digite sua renda salarial: ');
+
+//     if (renda >= 3036) {
+//         console.log('Você está apto.');
+//     } else {
+//         console.log('Você não tem renda suficiente.');
+        
+//     }
+// } else {
+//     console.log('Infelizmente você não tem a idade necessária');
+    
 // }
 
 // 5. Em um concurso para professor em uma instituição de ensino, os seguintes critérios são considerados 
@@ -50,37 +68,36 @@
 	// Faça um script em JavaScript que solicite a experiência e a formação do candidato, e calcule e exiba qual 
     // seria a sua pontuação para o concurso.
 
-// let experiencia = prompt('Digite sua experiencia profissional:');
-// let formacao = prompt('Digite sua formação(superior, pos, mestrado ou doutorado):');
+// let experiencia = prompt('Digite sua experiencia profissional em anos:');
 
-// let pontosExp = 0;
-// let pontosFor = 0;
-// let totalPontos = 0;
+// let pontos = 0;
+
 
 // if (experiencia <= 4) {
-//     pontosExp = 10;
-//     console.log('recebeu 10 pontos.')
+//     pontos += 10;
+//     console.log('recebeu 10 pontos.');
 // } else if (experiencia >= 5){
-//     pontosExp = 20;
-//     console.log('recebeu 20 pontos.')
+//     pontos += 20;
+//     console.log('recebeu 20 pontos.');
 // }
+
+// let formacao = prompt('Digite sua formação(superior, pos, mestrado ou doutorado):');
 
 // if (formacao == 'superior') {
-//     pontosFor = 10
-//     console.log('recebeu 10 pontos')
+//     pontos += 10
+//     console.log('recebeu 10 pontos');
 // } else if (formacao == 'pos') {
-//     pontosFor = 20
-//     console.log('recebeu 20 pontos.')
+//     pontos += 20
+//     console.log('recebeu 20 pontos.');
 // } else if (formacao == 'mestrado') {
-//     pontosFor = 30
-//     console.log('recebeu 30 pontos.')
+//     pontos += 30
+//     console.log('recebeu 30 pontos.');
 // } else if (formacao == 'doutorado') {
-//     pontosFor = 40
-//     console.log('recebeu 40 pontos.')
+//     pontos += 40
+//     console.log('recebeu 40 pontos.');
 // }
 
-// totalPontos = pontosFor + pontosExp;
-// console.log('Seus pontos foram: ', totalPontos);
+// console.log(`Seus pontos foram: ${pontos}`);
 
 
 // 6. Faça um script em JavaScript que solicite uma nota ao usuário e verifique se ela está entre 7.0 e 10.0.
@@ -122,27 +139,27 @@
 // Faça um script em JavaScript que solicite a renda familiar e a média acadêmica do estudante, calcule e exiba 
 // qual seria a sua pontuação total para concorrer à bolsa de estudos.
 
-let renda = prompt('Digite sua renda familiar: ');
-let media = prompt('Digite sua média: ');
+// let renda = prompt('Digite sua renda familiar: ');
+// let media = prompt('Digite sua média: ');
 
-let pontosRenda = 0;
-let pontosMedia = 0;
+// let pontosRenda = 0;
+// let pontosMedia = 0;
 
-if (renda <= 2000) {
-    pontosRenda = 30;
-} else if (renda >= 2001 && renda <= 4000) {
-    pontosRenda = 20;
-} else if (renda > 5000) {
-    pontosRenda = 10;
-}
+// if (renda <= 2000) {
+//     pontosRenda = 30;
+// } else if (renda >= 2001 && renda <= 4000) {
+//     pontosRenda = 20;
+// } else if (renda > 5000) {
+//     pontosRenda = 10;
+// }
 
-if (media <= 6.9) {
-    pontosMedia = 5;
-} else if (media >= 7 && media <= 8.4) {
-    pontosMedia = 15;
-} else if (media > 8.5 && media < 10) {
-    pontosMedia = 25;
-}
+// if (media <= 6.9) {
+//     pontosMedia = 5;
+// } else if (media >= 7 && media <= 8.4) {
+//     pontosMedia = 15;
+// } else if (media > 8.5 && media < 10) {
+//     pontosMedia = 25;
+// }
 
-let totalPontos = pontosRenda + pontosRenda;
-console.log('Sua pontuação foi: ', totalPontos);
+// let totalPontos = pontosRenda + pontosRenda;
+// console.log('Sua pontuação foi: ', totalPontos);
