60d6f0f29354ee2ee3fd8f92dba4910a809fc6ae
juan-paiva <juanpaiva.ym@gmail.com>
2025-09-29 19:13:54 -0300

    Exercícios de DOM

---
+++ exercicios-aluno/DOM/js/main.js
@@ -0,0 +1,8 @@
+let cortico = document.getElementById('o-cortico');
+console.log(cortico);
+
+let elementsA = document.getElementsByTagName('a');
+console.log(elementsA)
+
+let vidasSecas = document.querySelector('#vidas-secas');
+console.log(vidasSecas);
